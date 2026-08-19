5c6c263ffa73f808da2637fee260a3554818878e
pashameed-art <pashameed@gmail.com>
2026-08-20 02:56:48 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='sale-profit-book-v2.9';
+const CACHE='sale-profit-book-v3.0';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET') return;
- e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
+ e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));
 });
