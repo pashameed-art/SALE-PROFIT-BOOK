7923f4f0b2bd1caa6fdb84526d183a22b85202f6
pashameed-art <pashameed@gmail.com>
2026-08-20 07:19:13 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,14 @@
-const CACHE='sale-profit-book-v4.25-pwa3';
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.webmanifest',
-      './icon-192.png',
-      './icon-512.png'
-    ])).then(() => self.skipWaiting())
-  );
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+const CACHE='sale-profit-book-v4.25-pwa-final';
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll([
+    './','./index.html','./manifest.json','./icon-192.png','./icon-512.png'
+  ])).then(()=>self.skipWaiting())
+));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  .then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
