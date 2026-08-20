ed1dce06f56f79932f982faf7296ace4ec8bb0dc
pashameed-art <pashameed@gmail.com>
2026-08-20 18:29:32 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sale-profit-book-v4.25-pwa-final';
+const CACHE='sale-profit-book-v4.48-pwa-final';
 self.addEventListener('install',e=>e.waitUntil(
   caches.open(CACHE).then(c=>c.addAll([
     './','./index.html','./manifest.json','./icon-192.png','./icon-512.png'
